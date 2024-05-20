479df7a7ddbc60be1ec900f5a30ff86a0e2fc650
polinkaaa <polina_kond@mail.ru>
2024-05-20 16:47:26 +0300

    add Content-Type in headers

--- client/src/http/userAPI.js
+++ client/src/http/userAPI.js
@@ -1,8 +1,12 @@
 import { $host, $authHost } from ".";
 import { jwtDecode } from "jwt-decode"
+import axios from "axios";
+const headers ={
+    'Content-Type': 'application/json'
+}/**//*$host*//**/
 
 export const registration = async (nickname, email, password) => {
-    const {data} = await $host.post('api/user/registration', {nickname, email, password, role: 'ADMIN'})
+    const {data} = await axios.post('http://localhost:5000/api/user/registration', {nickname, email, password, role: 'ADMIN'}, headers)
     return jwtDecode(data.token)
 }
 export const login = async (email, password) => {
